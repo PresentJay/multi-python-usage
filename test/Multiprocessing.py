b1dbbda6c985aa9e314d3b2e5d917e1f09c514fd
presentj94 <presentj94@gmail.com>
2021-06-11 16:46:21 +0900

    feat: reinforcing monitoring codes

--- test/Multiprocessing.py
+++ test/Multiprocessing.py
@@ -1,12 +1,21 @@
 import time
-from utils.monitoring import show_short_info, show_full_cpuinfo
+import itertools
+import multiprocessing
+from utils.monitoring import show_short_info
 
 """ Sequential nested loop test """
 
 def test(iter1 = 1000, iter2 = 800, iter3 = 800, benchmark=20000000):   
+    
+    z = range(iter1)
+    y = range(iter2)
+    x = range(iter3)
+    
     start = time.perf_counter()
     temp = time.time()
     cnt = 0
+    
+    
     
     for z in range(iter1):
         for y in range(iter2):
@@ -14,9 +23,8 @@ def test(iter1 = 1000, iter2 = 800, iter3 = 800, benchmark=20000000):
                 cnt+=1
                 if cnt%benchmark == 0:
                     temp = time.time() - temp
-                    print(f'{(cnt / (iter1 * iter2 * iter3) * 100):05.2f}% progress : elapsed time {time.perf_counter()-start:05.2f} s, ', end='')
+                    print(f'{(cnt / (iter1 * iter2 * iter3) * 100):.2f}% progress : elapsed time {time.perf_counter()-start:.2f} s, ', end='')
                     show_short_info()
-                    show_full_cpuinfo()
                     
                     temp = time.time()
     
--- utils/monitoring.py
+++ utils/monitoring.py
@@ -6,17 +6,26 @@ import psutil
 def show_short_info():
     Gpus = GPUtil.getGPUs()
     for gpu in Gpus:
-        print(f'[GPU({gpu.id})|{gpu.memoryUtil*100:.2f}%]',end=' ')
+        print(f'[GPU({gpu.id}) Usage {gpu.load*100:.2f}%|VRAM {gpu.memoryUtil*100:.2f}%]',end=' ')
     mem=psutil.virtual_memory()
-    print(f'[CPU|{mem.percent}%]')
+    print(f'[CPU Usage {psutil.cpu_percent():.2f}%|RAM {mem.percent:.2f}%]')
     
+    return gpu.memoryUtil*100, mem.percent
+
+def show_full_cpuinfo():
+    cpu=psutil.cpu_percent(percpu=True)
+    for core in cpu:
+        print(f'[{core:05.2f}%]', end=' ')
+    print('\n')
+    return cpu
+
 
 def get_gpu_info():
     Gpus = GPUtil.getGPUs()
     gpulist = []
     # GPUtil.showUtilization() 
     for gpu in Gpus:
-        print(f'[GPU] {gpu.name}\t(index {gpu.id} /{len(Gpus)-1} total) \n\t==> {gpu.memoryUsed/1024:2.2f} GB/ {gpu.memoryTotal/1024:2.2f} GB({gpu.memoryUtil*100:2.2f}%)')
+        print(f'[GPU] {gpu.name}\t(index {gpu.id} /{len(Gpus)-1} total) | Usage {gpu.load*100:.2f}% \n\t==> [VRAM {gpu.memoryUsed/1024:2.2f} GB/ {gpu.memoryTotal/1024:2.2f} GB({gpu.memoryUtil*100:2.2f}%)]')
         
         # Press# to add information one by one GPU
         gpulist.append([ gpu.id, gpu.memoryTotal, gpu.memoryUsed,gpu.memoryUtil * 100])
@@ -24,20 +33,22 @@ def get_gpu_info():
     return gpulist
 
 
+# TODO: 코어별 사용량 변동폭을 기록할만한 것 같음
 def get_cpu_info():
     cpu = cpuinfo.get_cpu_info()
     mem = psutil.virtual_memory()
     memtotal = mem.total
     mempercent = mem.percent
     memused = mem.used
-    cpuPercent = psutil.cpu_percent(percpu=True)
-    print(f'[CPU] {cpu["brand_raw"]}\t[{cpu["count"]} cores] \n\t==> {memused/1024/1024/1024:.2f} GB/ {memtotal/1024/1024/1024:.2f} GB({mempercent}%)')
-    for index, core in enumerate(cpuPercent):
+    totalUsage = psutil.cpu_percent()
+    coreUsage = psutil.cpu_percent(percpu=True)
+    print(f'[CPU] {cpu["brand_raw"]}\t[{cpu["count"]} cores] [Usage {totalUsage:.2f}%] \n\t==> {memused/1024/1024/1024:.2f} GB/ {memtotal/1024/1024/1024:.2f} GB({mempercent}%)')
+    for index, core in enumerate(coreUsage):
         print(f'[{index:2d}]{core:05.2f}%', end='  ')
         if (index+1)%8==0:
             print('')
     print('')
-    return memtotal, memused, mempercent, cpuPercent
+    return coreUsage, totalUsage, mempercent, mempercent
 
 
 def show_monitor_iter(max_iter=10000000, delay=2):
@@ -60,8 +71,9 @@ def show_monitor_iter(max_iter=10000000, delay=2):
 def show_info():
     time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
     now = time.localtime()
-    print("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
+    print("\n\t- - - - %04d/%02d/%02d %02d:%02d:%02d - - - -" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
     
-    cpu_info = get_cpu_info()
+    cpu_core_usage, cpu_total_usage, cpu_mem = get_cpu_info()
     gpu_info = get_gpu_info()
     print('- - - -')
+    
